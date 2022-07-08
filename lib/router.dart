cb5d3bd63543805bd2e9d06a2f105db5989b481d
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-07-09 01:29:00 +0530

    auth screen ui

---
+++ lib/router.dart
@@ -0,0 +1,22 @@
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routeName:
+      return MaterialPageRoute(
+        builder: (_) => const AuthScreen(),
+      );
+
+    default:
+      return MaterialPageRoute(
+        builder: (_) => const Scaffold(
+          body: Center(
+            child: Text(
+              'Page Not Found!',
+            ),
+          ),
+        ),
+      );
+  }
+}
